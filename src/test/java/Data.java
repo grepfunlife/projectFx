1a800f797de22f1faa54182d5eccfc693780d9dd
polanskaya <polanskaya@yamoney.ru>
2018-10-25 02:37:23 +0300

    Remove files

--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -2,6 +2,7 @@ import org.testng.annotations.DataProvider;
 
 public class Data {
 
+
     @DataProvider(name = "data-provider")
     public static Object[][] dataProviderMethod() {
         return new Object[][]{{4, "0.25"}, {0, "х не должен быть равен нулю"}, {500.1, "х не может быть больше 500"},
