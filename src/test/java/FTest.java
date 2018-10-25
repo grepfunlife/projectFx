7557aec7995715ba16101558a4969a08e0b71e1f
polanskaya <polanskaya@yamoney.ru>
2018-10-25 20:42:58 +0300

    ..

--- src/test/java/FTest.java
+++ src/test/java/FTest.java
@@ -2,6 +2,7 @@ import org.testng.annotations.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -10,6 +11,7 @@ public class FTest {
     protected  ByteArrayOutputStream output;
     private PrintStream old;
 
+    //здесь я подменяю поток вывода в консоль на свой перед запуском теста с очередным набором данных
     @BeforeMethod
     public void setUpStreams() {
         old = System.out;
@@ -26,5 +28,12 @@ public class FTest {
     public void fTest(double x, String expResult) {
         Fx.fx(x);
         assertEquals(expResult, output.toString());
+    }
+
+    //Тут я осознаю, что тест ловит не тот эксепшен, который хотелось бы, но сделала, как пример того, что так можно делать )
+    @Test(expectedExceptions = Exception.class)
+    public void ExTest() {
+        Double x = Double.valueOf("rr");
+        Fx.fx(x);
     }
 }
