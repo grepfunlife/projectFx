e55fef2adc963d3a91278aac7864e3dd03a0fb83
polanskaya <polanskaya@yamoney.ru>
2018-10-25 07:12:41 +0300

    ..

--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -2,11 +2,14 @@ import org.testng.annotations.DataProvider;
 
 public class Data {
 
-
     @DataProvider(name = "data-provider")
     public static Object[][] dataProviderMethod() {
-        return new Object[][]{{4, "0.25"}, {0, "х не должен быть равен нулю"}, {500.1, "х не может быть больше 500"},
-                {-500.1, "х не может быть меньше -500"}, {500, "0.002"}, {-500, "-0.002"}};
-
+        return new Object[][]{
+                {4, "0.25"},
+                {0, "х не должен быть равен нулю"},
+                {500.1, "х не может быть больше 500"},
+                {-500.1, "х не может быть меньше -500"},
+                {500, "0.002"},
+                {-500, "-0.002"}};
     }
 }
